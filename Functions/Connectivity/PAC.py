e851f5c8e405e9a7843f74e4e5775c6a9498bf80
Matin <matinarmanmehr@proton.me>
2024-08-08 00:57:32 +0330

    Finished the PAC codes for time series and fixed ts_utils.py

--- Functions/Connectivity/PAC.py
+++ Functions/Connectivity/PAC.py
@@ -2,24 +2,57 @@ import numpy as np
 import scipy as sp
 import pywt
 import sys
+from tqdm import tqdm
 
 sys.path.insert(0, '../Utils/')
 import ts_utils
 
-sig_ph = np.random.randn(1000)
-sig_amp = np.random.randn(1000)
-fph = [4,8]
-famp = [20,80]
-Fs = 2000
+def PAC_varTime_ts(sig_ph, sig_amp, Fs, twin = 1, tovp = 0.8, fph = [3,8], famp = [20,80], nins = 9, fres_param = 1, ifProgBar = True):
 
-def PAC_varTime_ts(sig_ph,sig_amp,Fs,fph,famp):
-    scales = np.arange(1, 128, 0.5)
+    nwin = np.round(twin*Fs)
+    novp = np.round(twin*Fs*tovp)
+
+    k = 128
+    scales = np.arange(1, k, fres_param)
+    f = pywt.scale2frequency(wavelet='morl',scale=scales)
+    while np.min(f) > fph[0] and np.shape(f[(f>=famp[0]) &(f<=famp[-1])])[0] < famp[-1]-fmap[0]+1:
+        k += 2
+        scales = np.arange(1, k)
+        f = pywt.scale2frequency(wavelet='morl',scale=scales)
+        if k > 2^15:
+            raise ValueError('Cannot find the best parameters for frequency resolution!')
+    
     tfd_ph, f = pywt.cwt(sig_ph, scales = scales ,wavelet='morl', sampling_period=1/Fs)
     if np.all(sig_ph==sig_amp):
         tfd_amp = tfd_ph
     else:
         tfd_amp, f = pywt.cwt(sig_amp, scales = scales ,wavelet='morl', sampling_period=1/Fs)
+    Phase = np.angle(tfd_ph[ (f>=fph[0]) & (f<=fph[-1]) ,:])
+    Amp = np.abs(tfd_amp[ (f>=famp[0]) & (f<=famp[-1]) ,:])
     
+    fph_vec = f[(f>=fph[0]) & (f<=fph[-1])]
+    famp_vec = f[(f>=famp[0]) & (f<=famp[-1])]
+    Lfph = len(fph_vec)
+    Lfamp = len(famp_vec)
+
+
+    window_idx = ts_utils.window_it(np.arange(0,sig_ph.shape[0]), nwin, novp)
+    Lt = window_idx.shape[0]
+    t = np.arange(0,len(sig_ph))/Fs
+    tout = t[np.int16(np.median(window_idx,axis=1))]*1e3
+
+    PAC = np.zeros([Lt,Lfph,Lfamp])
+
+    if ifProgBar:
+        pb = range(Lt)
+        print('PAC calculation:')
+        for ti in tqdm(pb):
+            PAC[ti,:,:] = PAC_MI(Phase, Amp, nbins)
+    else:
+        for ti in range(Lt):
+            PAC[ti,:,:] = PAC_MI(Phase, Amp, nbins)
+    
+    return PAC, tout, fph_vec, famp_vec
 
 def PAC_MI(Phase, Amp, nbins):
     
--- Functions/Utils/ts_utils.py
+++ Functions/Utils/ts_utils.py
@@ -7,12 +7,12 @@ def window_it(sig, twin, tovp):
     Parameters:
     - sig: 1D array-like, the time series data.
     - twin: int, the size of each window.
-    - tovp: float, the percentage of overlap between windows (0 to 100).
+    - tovp: float, the number of samples for overlap between windows (0 to 1).
     
     Returns:
     - A 2D NumPy array where each row is a window of the time series.
     """
-    step_size = int(twin * (1 - tovp / 100.0))
+    step_size = int(twin - tovp)
     num_windows = (len(sig) - twin) // step_size + 1
     
     windows = np.array([sig[i * step_size : i * step_size + twin] for i in range(num_windows)])
