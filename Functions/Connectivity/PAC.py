d66ccb926a8cb1febd65c3d4b9a303f146a8c9d4
Matin <matinarmanmehr@proton.me>
2024-08-23 15:59:28 +0330

    Improved the pace of running PAC function

--- Functions/Connectivity/PAC.py
+++ Functions/Connectivity/PAC.py
@@ -1,20 +1,21 @@
 import numpy as np
 import scipy as sp
 import pywt
-from tqdm import tqdm
+from joblib import Parallel, delayed
+from copy import deepcopy
 from ..Utils import ts_utils
 
-def PAC_varTime_ts(sig_ph, sig_amp, Fs, twin = 1, tovp = 0.8, fph = [3,8], famp = [20,80], nbins = 9, fres_param = 1, ifProgBar = True):
+def PAC_varTime_ts(sig_ph, sig_amp, Fs, twin = 1, tovp = 0.5, fph = [3,8], famp = [20,80], nbins = 9, fres_param = 3.5):
 
-    nwin = np.round(twin*Fs)
-    novp = np.round(twin*Fs*tovp)
+    nwin = int(np.round(twin*Fs))
+    novp = int(np.round(twin*Fs*tovp))
 
     k = 100
     scales = np.arange(1, k, fres_param)
     f = pywt.scale2frequency(wavelet='morl',scale=scales) * Fs
-    while (np.min(f) > fph[0]) or (np.shape(f[(f>=famp[0]) &(f<=famp[-1])])[0] < (famp[-1]-famp[0]+1)):
+    while f[-1] > fph[0]:
         k += 10
-        scales = np.arange(1, k)
+        scales = np.arange(1, k, fres_param)
         f = pywt.scale2frequency(wavelet='morl',scale=scales) * Fs
         if (k > 2**14) or (fres_param <= 0):
             raise ValueError('Cannot find the best parameters for frequency resolution!')
@@ -24,14 +25,16 @@ def PAC_varTime_ts(sig_ph, sig_amp, Fs, twin = 1, tovp = 0.8, fph = [3,8], famp 
         tfd_amp = tfd_ph
     else:
         tfd_amp, f = pywt.cwt(sig_amp, scales = scales ,wavelet='morl', sampling_period=1/Fs)
-    Phase = np.angle(tfd_ph[ (f>=fph[0]) & (f<=fph[-1]) ,:])
-    Amp = np.abs(tfd_amp[ (f>=famp[0]) & (f<=famp[-1]) ,:])
     
-    fph_vec = f[(f>=fph[0]) & (f<=fph[-1])]
-    famp_vec = f[(f>=famp[0]) & (f<=famp[-1])]
+    idx_ph = (f>=fph[0]) & (f<=fph[-1])
+    idx_amp = (f>=famp[0]) & (f<=famp[-1])
+    Phase = np.angle(tfd_ph[idx_ph,:])
+    Amp = np.abs(tfd_amp[idx_amp,:])
+    
+    fph_vec = f[idx_ph]
+    famp_vec = f[idx_amp]
     Lfph = len(fph_vec)
     Lfamp = len(famp_vec)
-
 
     window_idx = ts_utils.window_it(np.arange(0,sig_ph.shape[0]), nwin, novp)
     Lt = window_idx.shape[0]
@@ -39,21 +42,14 @@ def PAC_varTime_ts(sig_ph, sig_amp, Fs, twin = 1, tovp = 0.8, fph = [3,8], famp 
     tout = t[np.int16(np.median(window_idx,axis=1))]*1e3
 
     PAC = np.zeros([Lt,Lfph,Lfamp])
+    PAC = Parallel(n_jobs=8)(delayed(PAC_MI)(Phase[:,window_idx[ti,:]], Amp[:,window_idx[ti,:]], nbins) for ti in range(Lt))
 
-    if ifProgBar:
-        pb = range(Lt)
-        print('PAC calculation:')
-        for ti in tqdm(pb):
-            PAC[ti,:,:] = PAC_MI(Phase[:,window_idx[ti,:]], Amp[:,window_idx[ti,:]], nbins)
-    else:
-        for ti in range(Lt):
-            PAC[ti,:,:] = PAC_MI(Phase[:,window_idx[ti,:]], Amp[:,window_idx[ti,:]], nbins)
-    
     return PAC, tout, fph_vec, famp_vec
 
 def PAC_MI(Phase, Amp, nbins):
     
-    Phase[Phase<0] = Phase[Phase<0] + 2*np.pi
+    Phase_new = deepcopy(Phase)
+    Phase_new[Phase_new<0] = Phase[Phase<0] + 2*np.pi
     Lfamp = np.shape(Amp)[0]
     Lfph = np.shape(Phase)[0]
     PAC = np.zeros([Lfph,Lfamp])
@@ -66,6 +62,7 @@ def PAC_MI(Phase, Amp, nbins):
                 if not (bin_amp.size == 0):
                     P[i] = np.mean(bin_amp)
             P = P/np.sum(P)
-            PAC[fpi,fai] = 1+np.sum(P[P!=0]*np.log2(P[P!=0]))/np.log2(nbins)
+            P = P[P!=0]
+            PAC[fpi,fai] = 1+np.sum(P*np.log2(P))/np.log2(nbins)
     
     return PAC
